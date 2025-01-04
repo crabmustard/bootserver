a9272b1e6c25b277cd9f5c53c4b99859540744f7
crabmustard <bryce.lippert@gmail.com>
2025-01-03 19:00:19 -0500

    Allowed methods on handlers

---
+++ handlers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileServerHits.Load()
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Reset metrics successful.\n"))
+}
--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+const filepathRoot = "."
+const port = "8080"
+
 func main() {
-	const filepathRoot = "."
-	const port = "8080"
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	srv := http.Server{
+	mux.Handle("/app/",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app",
+			http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerGetMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerResetMetrics)
+
+	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
@@ -19,4 +31,11 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
