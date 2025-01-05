b0e7188a22ff5b3531a01feb9437b07d4395b1f9
crabmustard <bryce.lippert@gmail.com>
2025-01-05 18:34:25 -0500

    clean chirp/validate len

--- handlers.go
+++ handlers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +18,8 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type validResp struct {
-		Valid bool `json:"valid"`
+		Valid bool   `json:"valid"`
+		Chirp string `json:"cleaned_body"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -26,11 +29,27 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if len(params.Body) > 140 {
-		respondWithError(w, http.StatusBadRequest, "chirp is too long")
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
 	}
+	cleaned := cleanChirp(params.Body)
 
 	respondWithJson(w, 200, validResp{
 		Valid: true,
+		Chirp: cleaned,
 	})
 
+}
+
+func cleanChirp(chirpBody string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	splitChirp := strings.Split(chirpBody, " ")
+	for i, word := range splitChirp {
+		lowWord := strings.ToLower(word)
+		if slices.Contains(badWords, lowWord) {
+			splitChirp[i] = "****"
+		}
+	}
+	cleaned := strings.Join(splitChirp, " ")
+	return cleaned
 }
