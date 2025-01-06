b733ef2fe269ebd66a5b66c07e7ef9ffff20ae7e
crabmustard <bryce.lippert@gmail.com>
2025-01-06 17:10:57 -0500

    startdb

--- handler_validate.go
+++ handler_validate.go
@@ -7,12 +7,6 @@ import (
 	"strings"
 )
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
 func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 	const maxChirpLength int = 140
 
--- metrics.go
+++ metrics.go
@@ -13,16 +13,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerGetMetrics(w http.ResponseWriter, r *http.Request) {
-	metricsHTML := `<html>
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`
+<html>
   <body style="background-color:grey;">
     <h1>Welcome, Chirpy Admin</h1>
     <p>Chirpy has been visited %d times!</p>
   </body>
-</html>`
-	hits := cfg.fileServerHits.Load()
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(metricsHTML, hits)))
+</html>`, cfg.fileServerHits.Load())))
 }
 
 func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
